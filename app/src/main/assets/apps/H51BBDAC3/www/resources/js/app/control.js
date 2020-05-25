cf58997341559c70b7e0157212083b85cdf66c4b
qianchenchn <qianchenchn@foxmail.com>
2020-05-25 11:30:38 +0800

    dcloud1.dat、dcloud2.dat 不需要

--- app/src/main/assets/apps/H51BBDAC3/www/resources/js/app/control.js
+++ app/src/main/assets/apps/H51BBDAC3/www/resources/js/app/control.js
@@ -27,3 +27,15 @@ function initSelect()
 			 }
 	   }
 }
+
+	function accAdd(arg1,arg2){ 
+		var r1,r2,m; 
+		try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0} 
+		try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0} 
+		m=Math.pow(10,Math.max(r1,r2)) 
+		return (arg1*m+arg2*m)/m 
+		} 
+		//给Number类型增加一个add方法，调用起来更加方便。 
+		Number.prototype.add = function (arg){ 
+		return accAdd(arg,this); 
+		}
