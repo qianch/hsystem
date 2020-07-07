e1e94353504c80ad03dcad6776a4df18a05283c6
qianchenchn <qianchenchn@foxmail.com>
2020-07-07 17:22:25 +0800

    app.js格式化

--- app/src/main/assets/apps/H51BBDAC3/www/resources/js/app/app.js
+++ app/src/main/assets/apps/H51BBDAC3/www/resources/js/app/app.js
@@ -7,89 +7,90 @@ App = {
 	 * APP ready方法，一般立即执行的方法，都要写到这里，类似JQuery
 	 * @param {Object} fn
 	 */
-	ready: function(fn) {
-		mui.init();
-		mui.plusReady(function() {
-			if(fn){
-			  fn();
-			}
-		});
-	},
-	preload: function(url, data,styles) {
-		return mui.preload({
-			url: url,
-			id: url,
-			extras: data,
-			styles:styles
-		});
-	},
-	show:function(webview){
-		webview.show(Config.animation.type,Config.animation.speed);
-	},
+    ready: function (fn) {
+        mui.init();
+        mui.plusReady(function () {
+            if (fn) {
+                fn();
+            }
+        });
+    },
+    preload: function (url, data, styles) {
+        return mui.preload({
+            url: url,
+            id: url,
+            extras: data,
+            styles: styles
+        });
+    },
+    show: function (webview) {
+        webview.show(Config.animation.type, Config.animation.speed);
+    },
 	/**
 	 * 页面跳转
 	 */
-	go: function(url, param, createNew) {
-		waiting("加载中", true);
-		var view;
-		view = plus.webview.create(url, url, null, param);
-		view.addEventListener("loaded", function() { //注册新webview的载入完成事件
-			closeWaiting();
-			view.show(Config.animation.type, Config.animation.speed); //把新webview窗体显示出来，显示动画效果为速度200毫秒的右侧移入动画
-		}, false);
-		return view;
-	},
+    go: function (url, param, createNew) {
+        waiting("加载中", true);
+        var view;
+        view = plus.webview.create(url, url, null, param);
+        view.addEventListener("loaded", function () { //注册新webview的载入完成事件
+            closeWaiting();
+            view.show(Config.animation.type, Config.animation.speed); //把新webview窗体显示出来，显示动画效果为速度200毫秒的右侧移入动画
+        }, false);
+        return view;
+    },
 	/**
 	 * 取参数值,需要在App.ready中才可以取到
 	 */
-	param: function(key) {
-		return plus.webview.currentWebview()[key];
-	},
+    param: function (key) {
+        return plus.webview.currentWebview()[key];
+    },
 	/**
 	 * AJAX GET方法
 	 * @param {Object} url
 	 * @param {Object} successCB
 	 * @param {Object} errorCB
 	 */
-	ajaxGet: function(url, successCB, errorCB) {
-		waiting("请稍后", true);
-		mui.ajax(url, {
-		    beforeSend:function(request){
-		       request.setRequestHeader("AppVersion",Config.project.version)
-		    },
-			type: "get",
-			dataType: "json",
-			timeout:30000,
-			success: function(data) {
-				closeWaiting();
-				if(successCB) {
-					successCB(data);
-				}
-			},
-			error: function(xhr, type, errorThrown) {
-				closeWaiting();
-				try{
-					var ret=JSON.parse(xhr.responseText);
-					tip(ret.error);
-				}catch(e){
-				}
+    ajaxGet: function (url, successCB, errorCB) {
+        waiting("请稍后", true);
+        mui.ajax(url, {
+            beforeSend: function (request) {
+                request.setRequestHeader("AppVersion", Config.project.version)
+            },
+            type: "get",
+            dataType: "json",
+            timeout: 30000,
+            success: function (data) {
+                closeWaiting();
+                if (successCB) {
+                    successCB(data);
+                }
+            },
+            error: function (xhr, type, errorThrown) {
+                closeWaiting();
+                try {
+                    var ret = JSON.parse(xhr.responseText);
+                    tip(ret.error);
+                } catch (e) {
+                    console.log(e);
+                }
 
-				if(errorCB){
-				   errorCB(xhr, type, errorThrown);
-				}
-			}
-		});
-	},
+                if (errorCB) {
+                    errorCB(xhr, type, errorThrown);
+                }
+            }
+        });
+    },
 	/**
 	 * 获取服务器地址
 	 */
-	getServerUrl:function(){
-		if(isEmpty(LS.get(KEY.SERVER_IP))){
-			return Config.serverUrl;
-		}else{
-			return Config.serverUrl.replace(SERVER_IP,LS.get(KEY.SERVER_IP));
-		}
-	},
+    getServerUrl: function () {
+        if (isEmpty(LS.get(KEY.SERVER_IP))) {
+            return Config.serverUrl;
+        } else {
+            return Config.serverUrl.replace(SERVER_IP, LS.get(KEY.SERVER_IP));
+        }
+    },
 	/**
 	 * AJAX GET方法
 	 * @param {Object} url 请求地址
@@ -97,52 +98,53 @@ App = {
 	 * @param {Object} successCB 成功回调
 	 * @param {Object} errorCB 失败回调
 	 */
-	ajaxPost: function(url, data, successCB, errorCB) {
-		waiting("请稍后", true);
-		mui.ajax(url, {
-		    beforeSend:function(request){
-		       request.setRequestHeader("AppVersion",Config.project.version)
-		    },
-			data: data,
-			type: "post",
-			timeout:30000,
-			dataType: "json",
-			success: function(data) {
-				closeWaiting();
-				if(successCB) {
-					successCB(data);
-				}
-			},
-			error: function(xhr, type, errorThrown) {
-				closeWaiting();
-				try{
-					var ret=JSON.parse(xhr.responseText);
-					tip(ret.error);
-				}catch(e){
-				}
+    ajaxPost: function (url, data, successCB, errorCB) {
+        waiting("请稍后", true);
+        mui.ajax(url, {
+            beforeSend: function (request) {
+                request.setRequestHeader("AppVersion", Config.project.version)
+            },
+            data: data,
+            type: "post",
+            timeout: 30000,
+            dataType: "json",
+            success: function (data) {
+                closeWaiting();
+                if (successCB) {
+                    successCB(data);
+                }
+            },
+            error: function (xhr, type, errorThrown) {
+                closeWaiting();
+                try {
+                    var ret = JSON.parse(xhr.responseText);
+                    tip(ret.error);
+                } catch (e) {
+                    console.log(e);
+                }
 
-				if(errorCB){
-				   errorCB(xhr, type, errorThrown);
-				}
-			}
-		});
-	},
+                if (errorCB) {
+                    errorCB(xhr, type, errorThrown);
+                }
+            }
+        });
+    },
 	/**
 	 * 获取当前登陆用户ID
 	 */
-	getLoginUserId: function() {
-		return LS.get(KEY.USER_ID);
-	},
+    getLoginUserId: function () {
+        return LS.get(KEY.USER_ID);
+    },
 	/**
 	 * 获取当前登陆用户名
 	 */
-	getLoginUserName: function() {
-		return LS.get(KEY.USER_NAME);
-	},
+    getLoginUserName: function () {
+        return LS.get(KEY.USER_NAME);
+    },
 	/**
 	 * 获取用户部门
 	 */
-	getUserDepartment:function(){
-		return LS.get(KEY.USER_DEPT);
-	}
+    getUserDepartment: function () {
+        return LS.get(KEY.USER_DEPT);
+    }
 };
--- app/src/main/assets/apps/H51BBDAC3/www/resources/js/app/control.js
+++ app/src/main/assets/apps/H51BBDAC3/www/resources/js/app/control.js
@@ -1,43 +1,48 @@
-function initSelect()
-	{
-		var  select_items=document.getElementsByTagName("select");
-		for(var i=0;i<  select_items.length;i++ )
-		{
-           var item = select_items[i];
-           getOption(item);
-	   }
+function initSelect() {
+    var select_items = document.getElementsByTagName("select");
+    for (var i = 0; i < select_items.length; i++) {
+        var item = select_items[i];
+        getOption(item);
+    }
 }
 
-    function getOption(item){
-       var dataoptions= item.getAttributeNode("data-options").value;
-       if(dataoptions!=null&&dataoptions!='')
-       {
-       	dataoptions="{"+dataoptions+"}";
-       	dataoptions=dataoptions.replace(/'/g, '"');
-       	var jsondata=JSON.parse(dataoptions);
-       		var valueField=jsondata["valueField"];
-       		var textField=jsondata["textField"];
-       		var url=jsondata["url"];
-       		App.ajaxGet("http://"+SERVER_IP + url, function(data) {
-       			if (data.length > 0) {
-       				var optionstring="";
-       				$.each(data,function(infoIndex,info){
-       					 optionstring += "<option value="+ info[valueField]+">"+ info[textField]+"</option>";
-       				     });
-       				item.innerHTML=optionstring;
-       				}
-       			});
-        }
+function getOption(item) {
+    var dataoptions = item.getAttributeNode("data-options").value;
+    if (dataoptions !== null && dataoptions !== '') {
+        dataoptions = "{" + dataoptions + "}";
+        dataoptions = dataoptions.replace(/'/g, '"');
+        var jsondata = JSON.parse(dataoptions);
+        var valueField = jsondata["valueField"];
+        var textField = jsondata["textField"];
+        var url = jsondata["url"];
+        App.ajaxGet("http://" + SERVER_IP + url, function (data) {
+            if (data.length > 0) {
+                var optionstring = "";
+                $.each(data, function (infoIndex, info) {
+                    optionstring += "<option value=" + info[valueField] + ">" + info[textField] + "</option>";
+                });
+                item.innerHTML = optionstring;
+            }
+        });
     }
+}
 
-	function accAdd(arg1,arg2){ 
-		var r1,r2,m; 
-		try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0} 
-		try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0} 
-		m=Math.pow(10,Math.max(r1,r2)) 
-		return (arg1*m+arg2*m)/m 
-		} 
-		//给Number类型增加一个add方法，调用起来更加方便。 
-		Number.prototype.add = function (arg){ 
-		return accAdd(arg,this); 
-		}
+function accAdd(arg1, arg2) {
+    var r1, r2, m;
+    try {
+        r1 = arg1.toString().split(".")[1].length
+    } catch (e) {
+        r1 = 0
+    }
+    try {
+        r2 = arg2.toString().split(".")[1].length
+    } catch (e) {
+        r2 = 0
+    }
+    m = Math.pow(10, Math.max(r1, r2));
+    return (arg1 * m + arg2 * m) / m
+}
+//给Number类型增加一个add方法，调用起来更加方便。
+Number.prototype.add = function (arg) {
+    return accAdd(arg, this);
+}
