f855d174340cb3fda123573128e739a2ebf31314
qianchenchn <qianchenchn@foxmail.com>
2020-07-15 10:46:26 +0800

    ersionName "4.9.2"

--- app/src/main/assets/apps/H51BBDAC3/www/resources/js/config.js
+++ app/src/main/assets/apps/H51BBDAC3/www/resources/js/config.js
@@ -1,7 +1,7 @@
 //var SERVER_IP="10.10.1.211:8090/mesTest";//测试服务器
-var SERVER_IP="10.10.1.9/mes";//生产环境
 //var SERVER_IP = "192.168.101.44/mes";//个人测试
-var EXTERNAL_IP = "122.225.2.62:11111/mesTest"; //外库映射IP
+var SERVER_IP="10.10.1.9/mes";//生产环境
+var EXTERNAL_IP = "122.225.2.62:11111/mes"; //外库映射IP
 var Config = {
 	/**
 	 * 服务器地址  
@@ -19,7 +19,7 @@ var Config = {
 		 * APP当前版本号
 		 */
 
-        version: "4.9.1"
+        version: "4.9.2"
     },
 	/**
 	 * 动画定义
